511fc52463c5a193e1fb6a3298e19d01a938c08a
yukyeongLee <yukyeong@kaist.ac.kr>
2018-12-13 17:37:21 +0900

    Update prob3.R
    add Rand index (related to cluster similarity)

--- Dataset/prob3.R
+++ Dataset/prob3.R
@@ -1,36 +1,54 @@
-# install.packages("factoextra") + NbClust
-library(factoextra) 
-library(NbClust)
-
-clin <- readRDS("C:/Users/Lee/Desktop/Finalterm-Project/clinical.rds") # data load
-mut <- readRDS("C:/Users/Lee/Desktop/Finalterm-Project/mutation.rds")
-gex <- readRDS("C:/Users/Lee/Desktop/Finalterm-Project/expression.rds")
-# genes <- read.csv("C:/Users/Lee/Desktop/Finalterm-Project/gex_anova_result.csv")
-# mutat <- read.csv("C:/Users/Lee/Desktop/Finalterm-Project/mut_chisq_result.csv")
+clin <- readRDS("C:/Users/YuKyeong/Desktop/Lecture/BiS335/final_PJ/Finalterm-Project/clinical.rds")
+gex <- readRDS("C:/Users/YuKyeong/Desktop/Lecture/BiS335/final_PJ/Finalterm-Project/expression.rds")
+mut <- readRDS("C:/Users/YuKyeong/Desktop/Lecture/BiS335/final_PJ/Finalterm-Project/mutation.rds")
+genes <- read.csv("C:/Users/YuKyeong/Desktop/Lecture/BiS335/final_PJ/Finalterm-Project/gex_anova_result.csv")
+mutat <- read.csv("C:/Users/YuKyeong/Desktop/Lecture/BiS335/final_PJ/Finalterm-Project/mut_chisq_result.csv")
 surv_ind <- clin$survival_index
-# genes <- genes[order(genes[,2]),]
-# mutat <- mutat[order(mutat[,2]),]
+genes <- genes[order(genes[,2]),]
+mutat <- mutat[order(mutat[,2]),]
 
+surv_ind <- clin$survival_index
 gex <- na.omit(gex)
 common_id <- intersect(intersect(clin$sample_id, mut$sample_id), colnames(gex)) # reduce data
 gex <- gex[, match( common_id,colnames(gex))]
-# mut <- mut[which(mut[,1] %in% common_id),c(1,2)]
-# mut <- mut[which(mut[,2] %in% mutat[,1]), ]
 
-# data <- data.frame(surv_ind = surv_ind[match(common_id, clin$sample_id)])
-# data <- cbind(data,t())
-
-set.seed(2) # kmean clustering
+set.seed(2)
 pca_gex <- prcomp(t(gex), scale = T) # pca 
 pve <- 100*pca_gex$sdev^2/sum(pca_gex$sdev^2)
 
-km.out <- kmeans(pca_gex$x, 2, nstart = 20)
 
-par(c(1,2)) # visualization silhouetter and etc. measure for optimal number of clusters
-fviz_nbclust(pca_gex$x, kmeans, method = "silhouette") 
-fviz_nbclust(pca_gex$x, kmeans, method = "wss")
-fviz_nbclust(pca_gex$x, kmeans, method = "gap_stat")
+# ---------- Hierarchial Clustering ----------
 
-# NbSluster function
-NbClust(data = pca_gex$x, distance = "euclidean",
-        min.nc = 2, max.nc = 15, method = "kmeans")
+library(factoextra) 
+library(NbClust)
+
+fviz_nbclust(pca_gex$x, hcut, method = "silhouette") 
+# avg. sil. width = 0.1 at k = 3
+
+library("fpc")
+
+hc.complete = hclust(dist(data[, -1]), method="complete")
+plot(hc.complete, main="Complete Linkage", xlab="", sub="", cex=.9)
+hc.cluster = cutree(hc.complete, 3)
+
+table(hc.cluster,as.numeric(data$surv_ind))
+# 112 12 129 55
+# 40 3 36 55
+# 22 0 3 13
+
+library(clues)
+adjustedRand(hc.cluster,as.numeric(data$surv_ind))
+# Rand : 0.52058977
+
+
+# ---------- K means ----------
+
+km.out <- kmeans(pca_gex$x, 3, nstart = 20)
+
+table(km.out$cluster, as.numeric(data$surv_ind))
+# 31 2 31 28
+# 110 12 123 68
+# 33 1 14 27
+
+adjustedRand(km.out$cluster, as.numeric(data$surv_ind))
+# Rand : 0.51083855
