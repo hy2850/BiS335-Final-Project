fd3d48289b013cf08ec4465e251a2e59e8bafa0f
yukyeongLee <yukyeong@kaist.ac.kr>
2018-12-15 02:00:10 +0900

    midtermprob5
    calculate MSE for mut

--- Dataset/midtermprob5.R
+++ Dataset/midtermprob5.R
@@ -7,23 +7,25 @@ gex <- na.omit(gex)
 clin <- na.omit(clin)
 
 id <- clin$sample_id
-with_mut_id <- mut$sample_id        # sample ids that have a mutation 
-unique_mut_gene <- unique(mut$Hugo_Symbol) # Remove redundant gene names by unique() function. 
-mut_hugo <- mut$Hugo_Symbol 
 
 exp_gene <- rownames(gex) 
 exp_sample <- colnames(gex) 
 
 common_pool <- intersect(intersect(id, with_mut_id), exp_sample)
+with_mut_id <- mut$sample_id # sample ids that have a mutation 
+unique_mut_gene <- unique(mut$Hugo_Symbol) # Remove redundant gene names by unique() function. 
+mut_Hugo <- mut$Hugo_Symbol
 
 stage <- clin$stage[match(common_pool, id)]
 subtype <- clin$subtype[match(common_pool, id)]
 surv <- clin$survival_time[match(common_pool, id)]
 gex <- gex[,match(common_pool, exp_sample)]
 
-# ---------- gene expression ----------
+####################
+## 1. Expression 
+####################
 
-getSigmoid <- function(surv, gex, a) {
+getSigmoidGene <- function(surv, gex, a) {
   
   # a : moving average (smoothing) width
   r <- c()
@@ -66,8 +68,8 @@ getSigmoid <- function(surv, gex, a) {
   return(data)
 }
 
-data.1st <- getSigmoid(surv, gex, 5)
-write.csv(data.1st, file = "sigmoid_1_a10.csv", row.names = T)
+sig.g.1st <- getSigmoidGene(surv, gex, 5)
+write.csv(sig.g.1st, file = "sigmoid_1_a10.csv", row.names = T)
 
 # # Plot gexp. - legit 
 # 
@@ -78,7 +80,7 @@ write.csv(data.1st, file = "sigmoid_1_a10.csv", row.names = T)
 # 
 # par(mfrow = c(3, 5))
 # for (i in 1:15){
-#   gene_expr <- gex[data.1st$number[i],] # i th gene expression
+#   gene_expr <- gex[sig.g.1st$number[i],] # i th gene expression
 # 
 #   temp <- surv[length(surv)+1-order(gene_expr, decreasing = TRUE)]
 #   temp <- convolve(temp, matrix(1/a, ncol = a), type = "filter")
@@ -101,9 +103,8 @@ write.csv(data.1st, file = "sigmoid_1_a10.csv", row.names = T)
 #   if(!is.na(r[i])){ #don`t save when r is NA
 #     rs[i] <- r[i]
 #   }
-#   plot(temp2, legit, cex = 0.5, main = rownames(data.1st)[i], xlab = "gene exp. level")
-#   text(x = min(temp2), y = 5, labels = paste("r =", round(data.1st$cor[i], 3)), pos = 4, col = 'blue')
-#   # Sys.sleep(1)
+#   plot(temp2, legit, cex = 0.5, main = rownames(sig.g.1st)[i], xlab = "gene exp. level")
+#   text(x = min(temp2), y = 5, labels = paste("r =", round(sig.g.1st$cor[i], 3)), pos = 4, col = 'blue')
 # }
 
 # sum((surv-mean(surv))^2)/length(surv)
@@ -117,7 +118,7 @@ write.csv(data.1st, file = "sigmoid_1_a10.csv", row.names = T)
 # gene_cor <- rownames(gex)[match(psm, ps)] #genes having p value lower then 0.05
 # gene_cor_h <- rownames(gex)[match(psm_holm, ps)] #genes having p value lower then 0.05 even after holm method
 
-plotMSE <- function(surv, gex, idx) {
+getMSEGene <- function(surv, gex, idx) {
   MSE <- c()
   minMSE <- c()
   thr_idx <- c()
@@ -152,9 +153,35 @@ plotMSE <- function(surv, gex, idx) {
   return(data)
 }
 
-data.MSE.1st <- plotMSE(surv, gex, data.1st$number[1:15])
+MSE.g.1st <- plotMSE(surv, gex, data.1st$number[1:15])
 
 
-# ---------- mutation ----------
 
+####################
+## 2. Mutation 
+####################
 
+getMSEMut <- function(surv, gene) {
+  MSE <- c()
+  O <- c()
+  X <- c()
+  
+  for (i in 1:length(unique_mut_gene)) {
+    mutO <- with_mut_id[mut_hugo == unique_mut_gene[i]] # id of samples with i th gene mutation  
+    mutX <- setdiff(with_mut_id, mutO) # id of samples without i th gene mutation
+    group1 <- na.omit(surv[match(mutO, common_pool)])
+    group2 <- na.omit(surv[match(mutX, common_pool)])
+    MSE <- c(MSE, (sum((group1-mean(group1))^2)/length(group1)+sum((group2-mean(group2))^2)/length(group2))/2)
+    O <- c(O, length(mutO))
+    X <- c(X, length(mutX))
+  }
+  
+  data <- data.frame(mutO = O, mutX = X, MSE = MSE) 
+  rownames(data) = unique_mut_gene
+  data <- data[order(MSE), ]
+  
+  return(data)
+}
+
+MSE.m.1st <- getMSEMut(surv, unique_mut_gene)
+MSE.m.1st.filter <- MSE.m.1st[MSE.m.1st$mutO>=20,]
